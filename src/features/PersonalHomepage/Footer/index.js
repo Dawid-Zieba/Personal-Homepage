ce42b11c6cdf1a88e23180127479ce4f8d72e76f
Dawid-Zieba <d.zieba2@gmail.com>
2023-06-15 18:04:29 +0200

    added footer

---
+++ src/features/PersonalHomepage/Footer/index.js
@@ -0,0 +1,17 @@
+import { SocialIcons } from "./SocialIcons";
+import { Wrapper, FooterContent, LetsTalk, Contact, Address, EmailWrapper } from "./styled";
+
+export const Footer = () => (
+    <Wrapper>
+        <LetsTalk>Let's talk!</LetsTalk>
+        <Address>
+            <EmailWrapper>
+                <Contact href="d.zieba2@gmail.com">d.zieba2@gmail.com</Contact>
+            </EmailWrapper>
+            <FooterContent>
+                I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me
+            </FooterContent>
+            <SocialIcons />
+        </Address>
+    </Wrapper>
+)
