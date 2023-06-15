4cea150c812b59b6eef680cb10a669e6980f47f8
Dawid-Zieba <d.zieba2@gmail.com>
2023-06-15 18:07:17 +0200

    added socials

--- src/features/PersonalHomepage/Footer/SocialIcons/socials.js
+++ src/features/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -1,2 +1,28 @@
-import { styleIcon } from "./styled";
+import { styledIcon } from "./styled";
 import { ReactComponent as FacebookIcon} from "./icons/github.svg";
+import { ReactComponent as InstagramIcon} from "./icons/instagram.svg";
+import { ReactComponent as GithubIcon} from "./icons/github.svg";
+import { ReactComponent as LinkedInIcon} from "./icons/linkedln.svg";
+
+export const socials = [
+    {
+        name: "GitHub",
+        url: "https://github.com/Dawid-Zieba",
+        Icon: styledIcon(GithubIcon),
+    },
+    {
+        name: "Facebook",
+        url: "https://www.facebook.com/dawid.zieba.549",
+        Icon: styledIcon(FacebookIcon),
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/yothiel5/",
+        Icon: styledIcon(InstagramIcon),
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/dawid-zi%C4%99ba-99b51827b/",
+        Icon: styledIcon(LinkedInIcon),
+    },
+];
