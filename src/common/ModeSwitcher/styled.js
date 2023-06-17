f02475b780b06b31cb3b13a78db8dbdef805c2f0
Dawid-Zieba <d.zieba2@gmail.com>
2023-06-17 10:44:06 +0200

    added background color to GlobalStyle

--- src/common/ModeSwitcher/styled.js
+++ src/common/ModeSwitcher/styled.js
@@ -2,23 +2,19 @@ import styled, {css} from "styled-components";
 import { ReactComponent as SunIcon } from "./sun.svg";
 
 export const Wrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
+    display: inline;
+    float: right;
 `;
 
 export const ThemeButton = styled.button`
     border: none;
     background-color: ${({ theme }) => theme.color.site.background};
     display: flex;
-    /* gap: 12px; */
     align-items: center;
-    /* margin-top: 60px;
-    margin-right: 14px;
-    transition: 0.5s; */
     color: inherit;
     outline-offset: 8px;
     transition: 0.4s;
-/* 
+
     &:hover {
         border: 1px solid ${({ theme }) => theme.color.modeSwitcher.border};
         border-radius: 4%;
@@ -31,13 +27,13 @@ export const ThemeButton = styled.button`
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         margin: 0;
         padding: 0;
-    } */
+    } 
 `;
 
 export const Text = styled.span`
     font-size: 12px;
     text-transform: uppercase;
-    font-weight: bold;
+    font-weight: 700;
     margin-right: 12px;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
@@ -55,14 +51,9 @@ export const Box = styled.span`
 `;
 
 export const IconWrapper = styled.div`
-    /* border: 1px solid ${({ theme }) => theme.color.modeSwitcher.border};
-    width: 48px;
-    height: 25px; */
     background-color: currentColor;
     border-radius: 50%;
     display: flex;
-    /* align-items: center;
-    padding: 2px; */
     transition: transform 0.3s;
     
     ${({ moveToRight }) => moveToRight && css`
--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -13,13 +13,17 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         font-family: 'Inter', sans-serif;
+        background-color: ${({theme}) => theme.color.site.background};
+        color: ${({ theme }) => theme.color.site.text};
         font-size: 18px;
         letter-spacing: 0.05em;
         word-break: break-word;
         transition: background 0.4s;
         padding-bottom: 108px;
 
-    
+        @media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px) {
+            padding-bottom: 32px;
+        }
         
     }
 
